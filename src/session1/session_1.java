409d916727fa1e2ec2f25f71c02260b9d11db653
ajita-asthana <ajita.asthana@gmail.com>
2022-06-22 21:08:27 -0400

    First Commit

---
+++ src/session1/session_1.java
@@ -0,0 +1,14 @@
+package session1;
+import java.io.*;
+
+public class session_1 {
+    public static void main(String args[])throws Exception{
+        BufferedReader ob = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the String: ");
+        String str = ob.readLine();
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        System.out.println(sb.toString());
+
+    }
+}
