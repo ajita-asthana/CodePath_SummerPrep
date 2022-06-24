f43297b6cecbb5e04321f9c94b3081a8f48dd1b4
ajita-asthana <ajita.asthana@gmail.com>
2022-06-23 20:40:28 -0400

    Bonums problem on next prime number

---
+++ src/session1/nextPrime.java
@@ -0,0 +1,32 @@
+package session1;
+import  java.io.*;
+public class nextPrime {
+    public static boolean isPrime(int n)
+    {
+        if(n==0 || n==1)
+            return false;
+
+        for(int i=2; i<=Math.sqrt(n); i++)
+        {
+            if(n%i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int nextPrime(int n)
+    {
+        while(!isPrime(n))
+            n++;
+        return n;
+    }
+    public static void main(String args[])throws Exception{
+        BufferedReader ob = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the number: ");
+        int n = Integer.parseInt(ob.readLine());
+        int res = nextPrime(n);
+        System.out.println("The result is "+res);
+    }
+}
