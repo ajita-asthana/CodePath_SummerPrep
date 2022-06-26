589642fc54d8f74784b71e42047f7a02ee168c6b
ajita-asthana <ajita.asthana@gmail.com>
2022-06-26 00:53:13 -0400

    Happy  Number

--- src/session2/happyNum.java
+++ src/session2/happyNum.java
@@ -1,9 +1,33 @@
 package session2;
 import java.io.*;
+import java.util.HashSet;
 
 public class happyNum {
     public static void main(String args[])throws Exception
     {
         BufferedReader ob = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the number: ");
+        int n = Integer.parseInt(ob.readLine());
+        HashSet<Integer> seen = new HashSet<>();
+        while(n!=1)
+        {
+            int current = n;
+            int sum = 0;
+            while(current != 0)
+            {
+                int x = current % 10;
+                sum += x*x;
+                current = current/10;
+            }
+
+            if(seen.contains(sum))
+            {
+                System.out.println("Number is not a happy number!!");
+                break;
+            }
+            seen.add(sum);
+            n = sum;
+        }
+        System.out.println("The number is a Happy Number");
     }
 }
